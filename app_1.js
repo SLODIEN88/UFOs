4e4488a6228fdb3c2388bcf1a83b8da9bc1dc15e
Sarah Lodien <96274446+SLODIEN88@users.noreply.github.com>
2022-03-16 22:45:18 -0500

    Add files via upload

--- app_1.js
+++ app_1.js
@@ -52,3 +52,37 @@ function doubleAddition(c, d) {
 
 doubleAddition = (c, d) => addition(c, d) * 2;
 doubleAddition = (33, 25 ) => addition (33, 25) *2;
+
+let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+function listLoop(userList) {
+  for (var i = 0; i < userList.length; i++) {
+    console.log(userList[i]);
+  }
+}
+listLoop(friends);
+
+var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+}
+
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+}
